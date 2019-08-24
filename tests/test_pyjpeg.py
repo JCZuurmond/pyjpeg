56cd3b36e6a22bc6650df0f5d529869030bf4c07
JCZuurmond <unknown>
2019-08-24 14:40:17 +0200

    Remove zero block fixture
    The zero block fixture was a little over kill. Therefore it is removed.

--- tests/test_pyjpeg.py
+++ tests/test_pyjpeg.py
@@ -4,23 +4,18 @@ import pytest
 from pyjpeg import pyjpeg
 
 
-@pytest.fixture
-def zeros_block():
-    return np.zeros((8, 8))
-
-
 def test_pyjpeg():
     assert pyjpeg
 
 
-def test_dct_zeros_block_all_zeros(zeros_block):
-    dct = pyjpeg.dct(zeros_block)
+def test_dct_zeros_block_all_zeros():
+    dct = pyjpeg.dct(np.zeros((8, 8)))
     assert (dct == 0).all()
 
 
-def test_idct_of_dct_is_identitcal_to_input(zeros_block):
-    idct = pyjpeg.idct(pyjpeg.dct(zeros_block))
-    assert np.array_equal(idct, zeros_block)
+def test_idct_of_dct_is_identitcal_to_input():
+    idct = pyjpeg.idct(pyjpeg.dct(np.zeros((8, 8))))
+    assert np.array_equal(idct, np.zeros((8, 8)))
 
 
 def test_dct_raises_value_error_wrong_patch_size():
