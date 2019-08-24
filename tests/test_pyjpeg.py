67a99024f2418866164ab909198da51a602233c5
JCZuurmond <unknown>
2019-08-24 14:42:55 +0200

    Use np.testing to check arrays

--- tests/test_pyjpeg.py
+++ tests/test_pyjpeg.py
@@ -10,12 +10,12 @@ def test_pyjpeg():
 
 def test_dct_zeros_block_all_zeros():
     dct = pyjpeg.dct(np.zeros((8, 8)))
-    assert (dct == 0).all()
+    np.testing.assert_array_equal(dct, 0)
 
 
 def test_idct_of_dct_is_identitcal_to_input():
     idct = pyjpeg.idct(pyjpeg.dct(np.zeros((8, 8))))
-    assert np.array_equal(idct, np.zeros((8, 8)))
+    np.testing.assert_array_equal(idct, np.zeros((8, 8)))
 
 
 def test_dct_raises_value_error_wrong_patch_size():
