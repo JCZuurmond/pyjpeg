2dff9be43261a144280ae4bab93ccdd0195f60d6
JCZuurmond <unknown>
2019-08-24 14:37:58 +0200

    Only expect 8 by 8 ndarrays

--- pyjpeg/pyjpeg.py
+++ pyjpeg/pyjpeg.py
@@ -2,33 +2,45 @@ import numpy as np
 from scipy import fftpack
 
 
-def dct(array: np.array) -> np.array:
+def dct(patch: np.ndarray) -> np.ndarray:
     """
-    Get the discrete cosine transform of the array.
+    Get the discrete cosine transform of the patch.
 
     Parameters
     ----------
-    array : np.array
-        The array of which the discrete cosine transform is taken.
+    patch : np.ndarray
+        The 8 by 8 patch of which the discrete cosine transform is taken.
 
     Returns
     -------
-    np.array : The discrete cosine transform of the array.
+    np.ndarray : The discrete cosine transform of the patch.
+
+    Raises
+    ------
+    ValueError : If the patch is not 8 by 8
     """
-    return fftpack.dct(array)
+    if not patch.shape == (8, 8):
+        raise ValueError(f'Patch should have shape (8, 8): patch.shape')
+    return fftpack.dct(patch)
 
 
-def idct(array: np.array) -> np.array:
+def idct(patch: np.ndarray) -> np.ndarray:
     """
-    Get the inverse discrete cosine transform of the array.
+    Get the inverse discrete cosine transform of the patch.
 
     Parameters
     ----------
-    array : np.array
-        The array of which the inverse discrete cosine transform is taken.
+    patch : np.ndarray
+        The patch of which the inverse discrete cosine transform is taken.
 
     Returns
     -------
-    np.array : The inverse discrete cosine transform of the array.
+    np.array : The inverse discrete cosine transform of the patch.
+
+    Raises
+    ------
+    ValueError : If the patch is not 8 by 8
     """
-    return fftpack.idct(array)
+    if not patch.shape == (8, 8):
+        raise ValueError(f'Patch should have shape (8, 8): patch.shape')
+    return fftpack.idct(patch)
--- tests/test_pyjpeg.py
+++ tests/test_pyjpeg.py
@@ -21,3 +21,13 @@ def test_dct_zeros_block_all_zeros(zeros_block):
 def test_idct_of_dct_is_identitcal_to_input(zeros_block):
     idct = pyjpeg.idct(pyjpeg.dct(zeros_block))
     assert np.array_equal(idct, zeros_block)
+
+
+def test_dct_raises_value_error_wrong_patch_size():
+    with pytest.raises(ValueError):
+        pyjpeg.dct(np.zeros((3, 3)))
+
+
+def test_idct_raises_value_error_wrong_patch_size():
+    with pytest.raises(ValueError):
+        pyjpeg.idct(np.zeros((3, 3)))
