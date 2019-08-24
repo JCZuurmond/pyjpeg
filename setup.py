ea19c68d805e19e0e24ebf0e461f2c3969d9f1ce
JCZuurmond <unknown>
2019-08-24 10:49:50 +0200

    Add setup script

---
+++ setup.py
@@ -0,0 +1,34 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+"""The setup script."""
+
+
+import setuptools
+
+
+with open('README.md') as readme_file:
+    readme = readme_file.read()
+
+
+requirements = []
+setup_requirements = []
+test_requirements = ['pytest']
+extra_requirements = {}
+
+
+setuptools.setup(
+    name='pyjpeg',
+    author='Cor Zuurmond',
+    author_email='jczuurmond@protonmail.com',
+    description='JPEG implementation in Python.',
+    url='https://github.com/JCZuurmond/pyjpeg',
+    license='Open source',
+    packages=setuptools.find_packages('pyjpeg'),
+    version='0.1.0',
+    install_requires=requirements,
+    setup_requires=setup_requirements,
+    test_suite='tests',
+    tests_require=test_requirements,
+    extras_require=extra_requirements,
+)
