039fd74132502b882bcb5c8e2c5a16bbca31581e
JCZuurmond <unknown>
2019-08-24 11:17:32 +0200

    Implement IDCT with scipy

--- pyjpeg/pyjpeg.py
+++ pyjpeg/pyjpeg.py
@@ -16,3 +16,19 @@ def dct(array: np.array) -> np.array:
     np.array : The discrete cosine transform of the array.
     """
     return fftpack.dct(array)
+
+
+def idct(array: np.array) -> np.array:
+    """
+    Get the inverse discrete cosine transform of the array.
+
+    Parameters
+    ----------
+    array : np.array
+        The array of which the inverse discrete cosine transform is taken.
+
+    Returns
+    -------
+    np.array : The inverse discrete cosine transform of the array.
+    """
+    return fftpack.idct(array)
--- tests/test_pyjpeg.py
+++ tests/test_pyjpeg.py
@@ -16,3 +16,8 @@ def test_pyjpeg():
 def test_dct_zeros_block_all_zeros(zeros_block):
     dct = pyjpeg.dct(zeros_block)
     assert (dct == 0).all()
+
+
+def test_idct_of_dct_is_identitcal_to_input(zeros_block):
+    idct = pyjpeg.idct(pyjpeg.dct(zeros_block))
+    assert np.array_equal(idct, zeros_block)
