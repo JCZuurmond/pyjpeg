51001834635569aa83c5d85bf6eeaffd567d9c5f
Cor Zuurmond <jczuurmond@protonmail.com>
2019-10-18 10:48:56 +0200

    Add test for bits_to_int and int_to_bits

--- tests/test_huffman.py
+++ tests/test_huffman.py
@@ -4,6 +4,39 @@ from numpy.testing import assert_array_equal
 from pyjpeg import huffman
 
 
+BITS_INT = {
+    '1111101': 125,
+    '1111110': 126,
+    '1111111': 127,
+    '1111': 15,
+    '1110': 14,
+    '1101': 13,
+    '1100': 12,
+    '1011': 11,
+    '1010': 10,
+    '1001': 9,
+    '0111': 7,
+    '1000': 8,
+    '0110': 6,
+    '0101': 5,
+    '0100': 4,
+    '0011': 3,
+    '0010': 2,
+    '0001': 1,
+    '0000': 0,
+}
+
+
+def test_bits_to_int():
+    for bits, int_ in BITS_INT.items():
+        assert huffman.bits_to_int(bits) == int_
+
+
+def test_int_to_bits():
+    for bits, int_ in BITS_INT.items():
+        assert huffman.int_to_bits(int_).zfill(len(bits)) == bits
+
+
 def test_encode_all_zeros():
     zeros = np.zeros(20)
     out = huffman.encode(zeros)
