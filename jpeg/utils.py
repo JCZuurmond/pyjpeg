3212162865639db1d559aed596b24d816aea91bc
Cor Zuurmond <jczuurmond@protonmail.com>
2020-02-25 20:31:00 +0100

    Add utils

---
+++ jpeg/utils.py
@@ -0,0 +1,23 @@
+import numpy as np
+
+
+def generate_patches(im: np.ndarray, *, patch_size: int = 8) -> np.ndarray:
+    """
+    Generates patches from a given image.
+
+    Parameters
+    ----------
+    im : np.ndarray
+        Image
+    patch_size, optional (default : 8)
+        Patch size.
+
+    Returns
+    -------
+    np.ndarray : A patch.
+    """
+    if len(im.shape) != 2:
+        raise ValueError(f'Expecting 2D image: {im.shape}')
+    for y in range(im.shape[0], patch_size):
+        for x in range(im.shape[1], patch_size):
+            yield im[y: y + patch_size, x: x + patch_size]
